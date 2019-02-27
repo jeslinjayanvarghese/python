25728f54a23f04b8c1dd1d4c0b6ab0338f6b88f1
jeslinjaisan <jeslinjaisan@gmail.com>
2019-02-27 12:34:12 +0530

    again

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "hello.."
+@app.route("/home")
+def home():
+    return "WELCOME TO MY HOME PAGE"
+@app.route("/about")
+def about():
+    return "About Me"
+@app.route("/contact")
+def contact():
+    return "CONTACT IS 9447996345"
 if(__name__=="__main__"):
     app.run()
